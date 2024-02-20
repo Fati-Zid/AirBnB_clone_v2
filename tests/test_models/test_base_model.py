890a58c92377822e7a703522f8283fc2f168cc12
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-02-20 06:34:56 -0800

    delete skiped updated_at function fail

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -90,7 +90,6 @@ class test_basemodel(unittest.TestCase):
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "Not applicable for file storage")
     def test_updated_at(self):
         """ """
         new = self.value()
