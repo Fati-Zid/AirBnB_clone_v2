1df2a075e52f852dd708f1b2e90f63195ae09fea
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-02-20 09:58:22 -0800

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- A database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- A new user hbnb_dev (in localhost) with password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
