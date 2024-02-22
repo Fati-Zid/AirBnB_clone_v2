dc2245e582f43e66caf59dd7d1116437978ec9d8
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-02-22 09:31:25 -0800

    Update user.py task 7

--- models/user.py
+++ models/user.py
@@ -13,23 +13,9 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-<<<<<<< HEAD
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
                            backref="user")
-=======
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-<<<<<<< HEAD
-    places = relationship('Place', cascade='all, delete' backref='user')
-    reviews = relationship('Review', cascade='all, delete' backref='user')
-=======
-    places = relationship('Place', cascade='all, delete', backref='user')
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    
->>>>>>> 80c254acfcdcca816045c7346fab87d9c735758e
->>>>>>> 45191c419fa45eb59f0d5b67ad4de6bcba7379ff
