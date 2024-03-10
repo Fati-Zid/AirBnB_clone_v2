ebf97034cd669fea7981ce35c73c87b4f647893b
F2Z <114442030+Fati-Zid@users.noreply.github.com>
2024-03-10 02:25:47 +0000

    refactor:

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,27 @@
 #!/usr/bin/env bash
-# Script to prepare my server environement
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file for testing Nginx configuration
-echo "<html><head><title>Test Page</title></head><body>Hello, this is a test page!</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership recursively to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
-nginx_alias="location /hbnb_static {\n    alias /data/web_static/current/;\n}"
-if grep -q "location /hbnb_static" "$nginx_config"; then
-    sudo sed -i "/location \/hbnb_static/,$ d" "$nginx_config"
-fi
-sudo sed -i "/server_name _;/a $nginx_alias" "$nginx_config"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
-
-exit 0
