2d2d766006751b3e07b40067999daf98f3219a70
F2Z <114442030+Fati-Zid@users.noreply.github.com>
2024-03-10 02:35:12 +0000

    Create 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    try:
+        # Create versions folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Get current timestamp
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        # Create archive file name
+        archive_name = "web_static_{}.tgz".format(timestamp)
+
+        # Compress web_static folder into the archive
+        local("tar -czvf versions/{} web_static".format(archive_name))
+
+        # Return the archive path if the archive has been correctly generated
+        return "versions/{}".format(archive_name)
+    except Exception as e:
+        # Print error message if any exception occurs
+        print("Error:", e)
+        return None
